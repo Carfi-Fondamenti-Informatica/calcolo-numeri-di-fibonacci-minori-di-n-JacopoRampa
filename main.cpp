71811c1182ffdf324daa8c4f822ef4cabd6a7e77
JacopoRampa <91597718+JacopoRampa@users.noreply.github.com>
2021-11-09 17:07:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int a=1,b=1,c=0,n=0;
+    cin >> n;
+    if(n>0){
+        cout << a << endl;
+        cout << b << endl;
+        c=a+b;
+        a=b;
+        b=c;
+        while(c<=n){
+            cout << c <<endl;
+            c=a+b;
+            a=b;
+            b=c;
+        }
+    }
+    return 0;
 }
